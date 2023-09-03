98023ecdb36bd3288832578561a07d9ad46848a6
Muratva Stark <48415026+muratvastark@users.noreply.github.com>
2023-09-03 20:31:02 +0300

    Update addStreamStat.ts

--- src/structures/addStreamStat.ts
+++ src/structures/addStreamStat.ts
@@ -1,5 +1,5 @@
 import { GuildModel, StatClass, UserStatModel } from '@/models';
-import { GuildMember, VoiceChannel } from 'discord.js';
+import { Colors, EmbedBuilder, GuildMember, TextChannel, VoiceChannel, inlineCode, roleMention } from 'discord.js';
 import { RankFlags } from 'src/enums/RankFlags';
 
 const ONE_DAY = 1000 * 60 * 60 * 24;
@@ -39,7 +39,22 @@ export async function addStreamStat(member: GuildMember, channel: VoiceChannel, 
             const oldRanks = guildData.ranks.filter((r) => member.roles.cache.has(r.role)).map((r) => r.role);
             if (oldRanks.length) member.roles.remove(oldRanks);
         }
-        if (!member.roles.cache.has(newRank.role)) member.roles.add(newRank.role);
+
+        if (!member.roles.cache.has(newRank.role)) {
+            member.roles.add(newRank.role);
+
+            const logChannel = member.guild.channels.cache.find(c => c.name === 'rank-up') as TextChannel;
+            if (logChannel) {
+                await logChannel.send({
+                    embeds: [
+                        new EmbedBuilder({
+                            color: Colors.Aqua,
+                            description: `${member} (${inlineCode(member.id)}) adlı kullanıcıya ${roleMention(newRank.role)} rolü verildi.`
+                        })
+                    ]
+                });
+            }
+        }
     }
 
     document.markModified('streams');
